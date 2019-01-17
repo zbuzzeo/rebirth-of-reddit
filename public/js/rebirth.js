b50d445114b1fd7e9ac6f20b40e82bf9d7eda6a2
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-01-16 14:31:43 -1000

    card containers and hover effect

---
+++ public/js/rebirth.js
@@ -0,0 +1,32 @@
+'use strict';
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                          CARD SETUP
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+function rebirthModule() {
+
+  return {
+
+  }
+}
+
+(function () {
+  console.log('hello world!');
+  const cardContainer = document.querySelector('#cardContainer');
+
+  const makeElement = (parent, type, className, content = '') => {
+    const element = document.createElement(type);
+    element.className = className;
+    element.innerHTML = content;
+    parent.appendChild(element);
+  }
+
+  const generateContainers = (amount) => {
+    for (let i = 1; i <= amount; i++) {
+      makeElement(cardContainer, 'div', 'cardBox');
+    }
+  }
+
+  generateContainers(24);
+})();
