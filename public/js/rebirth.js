0d371ba4f2a199157ff9d72b6c788adb63c23cc6
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-01-16 15:41:34 -1000

    card layout tweaks

--- public/js/rebirth.js
+++ public/js/rebirth.js
@@ -1,4 +1,9 @@
 'use strict';
+// const sampleTexts = require('../js/sampleTexts');
+// const randomSample = sampleTexts.randomSample(sampleTexts.samples);
+
+const sampleText = `Flax seed crackers burning man open-minded root chakra, Bay Area sun salutation blessed tie-dye chanting soulmate. Family constellation Whole Earth Catalog my brothers and sisters, deep tincture. Promoting positive change sound healing fluorescent lights all-encompassing, heartbeat of our ancestors east-west rawthentic self sacred ceremony Phish stevia. Reflexology tantra co-create, reprogramming your DNA mystic. Psychic vitamin-rich radiant, white sage agave. Namaste.`;
+
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *                          CARD SETUP
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -32,10 +37,10 @@ function rebirthModule() {
     const cardBoxes = document.querySelectorAll('.cardBox');
     
     cardBoxes.forEach(box => {
-      makeElement(box, 'img', 'postThumbnail');
-      makeElement(box, 'div', 'postTitle');
-      makeElement(box, 'div', 'postDetails');
-      makeElement(box, 'div', 'postPreview');
+      makeElement(box, 'div', 'postThumbnail');
+      makeElement(box, 'div', 'postTitle', sampleText);
+      makeElement(box, 'div', 'postDetails', sampleText);
+      makeElement(box, 'div', 'postPreview', sampleText);
     });
   }
 
---
+++ public/js/sampleTexts.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function textModule() {
+  samples = [
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tristique dictum nunc quis condimentum. Cras facilisis ultrices ante quis sollicitudin. Maecenas vitae tortor enim. In eu tortor rhoncus, cursus nisi eu, sodales arcu. Ut suscipit ornare dolor a sagittis. Vivamus et est sit amet ex feugiat sodales ac id magna. Nulla vestibulum, enim a suscipit fringilla, justo lectus ultricies turpis, ac blandit turpis diam et quam.`,
+
+    `Vivamus non vestibulum metus. Fusce sodales erat at magna ullamcorper ullamcorper. Duis mollis urna et arcu ornare facilisis. Donec felis tellus, pretium id turpis sed, vestibulum facilisis dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin ultricies magna quis lacus viverra pretium. Nam imperdiet metus sit amet eros efficitur, vitae fermentum leo malesuada. Integer congue, erat a malesuada efficitur, ligula nulla maximus neque, maximus egestas magna dui vitae odio. In hac habitasse platea dictumst. Phasellus malesuada lacus libero, sed laoreet sem mattis quis. Sed eget ornare ligula, eget tincidunt ex.`,
+
+    `Donec id scelerisque ante. Donec augue sem, condimentum a pellentesque eu, finibus sit amet eros. Sed scelerisque nunc rutrum est sollicitudin ultrices. Sed lorem turpis, tincidunt nec egestas eu, vestibulum sed leo. Maecenas tempus eros nibh, at congue ipsum tincidunt quis. Donec vitae hendrerit neque, sit amet pharetra magna. Donec convallis, urna dictum venenatis commodo, turpis lectus aliquam sapien, sit amet hendrerit nunc leo at justo. Vivamus sit amet ligula sed mauris semper semper at facilisis lorem. Integer sit amet bibendum metus. Curabitur id venenatis dui. Sed id pulvinar velit, et tincidunt tellus.`,
+
+    `Suspendisse malesuada tristique diam in cursus. In non sodales lacus, eu commodo velit. Quisque lobortis dolor odio, vel sollicitudin enim molestie id. Cras eu nibh vel risus scelerisque malesuada. Nulla arcu tellus, accumsan sit amet tempus sed, lobortis a massa. Praesent porta non mi a molestie. Nam convallis lorem vel erat accumsan volutpat. Nullam tempus mattis ex, eu maximus erat euismod fermentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam id libero in libero tincidunt vehicula quis eu mauris. Proin non metus et felis congue aliquam. Phasellus ut mollis magna, quis malesuada nunc. Mauris sit amet turpis ligula. Vivamus suscipit bibendum auctor. Donec semper sem consectetur, vestibulum augue eu, volutpat urna.`,
+
+    `Aenean eleifend rutrum ultricies. Mauris consectetur ex a condimentum posuere. Ut ac lorem gravida turpis posuere pretium. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi aliquet sapien ut enim mollis, vitae facilisis orci aliquam. Aliquam pharetra quam augue, a efficitur orci congue in. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam lacinia, enim mollis semper varius, ipsum arcu maximus tellus, id fermentum leo odio in arcu. Mauris pretium tempor mi sed lobortis. Vestibulum aliquet gravida imperdiet. Suspendisse vel mollis lacus, ac cursus diam.`
+  ]
+
+  const randomSample = (array) => {
+    console.log('hello world');
+  }
+
+  return {
+    samples : samples,
+    randomSample : randomSample
+  }
+}
